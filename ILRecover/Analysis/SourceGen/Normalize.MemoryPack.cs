c32ee11b42dcc810ca9e0e365635c35bcdf85e0c
Deathemonic <zinth.chan@tuta.io>
2026-06-09 17:39:14 +0800

    fix: normalize memorypack attributes

--- ILRecover/Analysis/SourceGen/Normalize.MemoryPack.cs
+++ ILRecover/Analysis/SourceGen/Normalize.MemoryPack.cs
@@ -2,6 +2,8 @@ using ICSharpCode.Decompiler.CSharp.Syntax;
 using ILRecover.Models;
 using ZLinq;
 
+using CSharpAttribute = ICSharpCode.Decompiler.CSharp.Syntax.Attribute;
+
 namespace ILRecover.Analysis.SourceGen;
 
 internal static partial class SourceGenNormalizer
@@ -16,6 +18,8 @@ internal static partial class SourceGenNormalizer
             if (!IsMemoryPackType(type))
                 continue;
 
+            type.Modifiers |= Modifiers.Partial;
+            RemoveDefaultMemoryPackGenerateTypeArguments(type);
             RemoveBaseTypes(type, "IMemoryPackable", "IMemoryPackFormatterRegister");
 
             foreach (var nestedType in type.Members.OfType<TypeDeclaration>().ToList())
@@ -29,6 +33,43 @@ internal static partial class SourceGenNormalizer
         || type.BaseTypes.Any(baseType =>
             AstTypeContains(baseType, "IMemoryPackable")
             || AstTypeContains(baseType, "IMemoryPackFormatterRegister"));
+
+    private static void RemoveDefaultMemoryPackGenerateTypeArguments(TypeDeclaration type)
+    {
+        foreach (var attribute in type.Attributes
+                     .AsValueEnumerable()
+                     .SelectMany(section => section.Attributes)
+                     .Where(attribute => AstTypeContains(attribute.Type, "MemoryPackable")))
+            RemoveDefaultMemoryPackGenerateTypeArgument(attribute);
+    }
+
+    private static void RemoveDefaultMemoryPackGenerateTypeArgument(CSharpAttribute attribute)
+    {
+        var arguments = attribute.Arguments.ToList();
+        if (arguments.Count != 1 || !IsDefaultMemoryPackGenerateTypeArgument(arguments[0]))
+            return;
+
+        arguments[0].Remove();
+        attribute.HasArgumentList = false;
+    }
+
+    private static bool IsDefaultMemoryPackGenerateTypeArgument(Expression argument) =>
+        IsGenerateTypeObject(argument)
+        || argument is AssignmentExpression { Operator: AssignmentOperatorType.Assign, Left: var left, Right: var right }
+            && IsMemoryPackGenerateTypeParameter(left)
+            && IsGenerateTypeObject(right);
+
+    private static bool IsMemoryPackGenerateTypeParameter(Expression expression) =>
+        expression is IdentifierExpression { Identifier: "GenerateType" or "GenerateType_" or "generateType" };
+
+    private static bool IsGenerateTypeObject(Expression expression) =>
+        expression is MemberReferenceExpression { MemberName: "Object", Target: var target }
+        && IsGenerateTypeReference(target);
+
+    private static bool IsGenerateTypeReference(Expression expression) =>
+        expression is IdentifierExpression { Identifier: "GenerateType" }
+        || expression is TypeReferenceExpression { Type: var type } && AstTypeContains(type, "GenerateType")
+        || expression is MemberReferenceExpression { MemberName: "GenerateType" };
 
     private static void RemoveBaseTypes(TypeDeclaration type, params string[] typeNames)
     {
